a03ab42be26a82ce635faa149fb98f03511099db
kjworld89 <4645448@naver.com>
2019-04-30 18:20:52 +0900

    LED 좌로/우로 shift

--- Src/main.c
+++ Src/main.c
@@ -130,12 +130,40 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   int index = 0;
+  uint8_t pattern = 0;
+  uint8_t shift = 0;
   while (1)
   {
-	  uint8_t pattern = 1 << index;
-	  index = (index + 1)% 8;
-	  ByteDataWrite(pattern);
-	  HAL_Delay(300);
+	  if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0) == 0)
+	  {
+		  shift = 0;
+	  }
+	  else if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1) == 0)
+	  {
+		  shift = 1;
+	  }
+	  if(shift == 0)
+	  {
+		  index++;
+		  if(index > 7)
+		  {
+			  index = 0;
+		  }
+		  pattern = 1 << index;
+		  ByteDataWrite(pattern);
+		  HAL_Delay(300);
+	  }
+	  else if(shift == 1)
+	  {
+		  index--;
+		  if(index < 0)
+		  {
+			  index = 7;
+		  }
+		  pattern = 1 << index;
+		  ByteDataWrite(pattern);
+		  HAL_Delay(300);
+	  }
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
@@ -215,9 +243,6 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_1, GPIO_PIN_RESET);
-
   /*Configure GPIO pins : PA0 PA1 PA2 */
   GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
@@ -227,9 +252,8 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pins : PB0 PB1 */
   GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
